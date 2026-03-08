0f87135995254fb889387ac7ccbd2cc554571064
krisnashoy88 <krisnashoy88@gmail.com>
2026-03-08 19:38:59 +0700

    Tugas Hello Next.js + Profile Krisna Imut

---
+++ my-app/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Halo teman teman semuanyaaa.</h1>
+      <h1>Ini Website Pertama Saya dengan Next.js</h1>
+
+      <p>Perkenalkan, saya Krisna Suryaningsih.</p>
+      <p>Mahasiswa D3 Teknik Informatika Politeknik Negeri Semarang.</p>
+
+      <a href="/profile">Lihat Profil Saya</a>
+    </main>
+  );
+}
---
+++ my-app/app/profile/page.tsx
@@ -0,0 +1,14 @@
+export default function Profile() {
+  return (
+    <main>
+      <h1>Profil Mahasiswa</h1>
+
+      <p>Nama: Krisna Suryaningsih</p>
+      <p>NIM: (3.34.24.3.12)</p>
+      <p>Program Studi: D3 Teknik Informatika</p>
+      <p>Minat: Web Development</p>
+
+      <a href="/">Kembali ke Home</a>
+    </main>
+  );
+}
